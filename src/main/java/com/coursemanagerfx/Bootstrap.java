71a2c3764235c1a47b26a029d392b3cafc715894
Nazuha <coolfr490@gmail.com>
2026-07-20 11:27:55 +0300

    update pom and loading all libs from maven

---
+++ src/main/java/com/coursemanagerfx/Bootstrap.java
@@ -0,0 +1,11 @@
+package com.coursemanagerfx;
+
+public final class Bootstrap {
+
+    private Bootstrap() {
+    }
+
+    public static void main(String[] args) {
+        Launcher.main(args);
+    }
+}
